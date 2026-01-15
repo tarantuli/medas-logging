a7e95348612012c3b9a050fee7113b42608a1298
Patrick Morpey <patrick@morphp.nl>
2026-01-15 09:51:10 +0100

    Added `alwaysAddClass` option to `StringMaker\Settings` in `VariableDumper` initialization

--- src/VariableDumper.php
+++ src/VariableDumper.php
@@ -26,7 +26,7 @@ readonly class VariableDumper
     {
         $directoryCreator->create($this->logDirectory);
 
-        $this->stringMakerSettings = new StringMaker\Settings(forceUtf8: true);
+        $this->stringMakerSettings = new StringMaker\Settings(forceUtf8: true, alwaysAddClass: true);
     }
 
     public function dump(mixed ...$variables): string
